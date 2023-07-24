9f3f9207cd69d84eee15fb0bf9d461a17e0dd526
kashishprajapati123 <ankushgahlot565@gmail.com>
2023-07-24 21:56:39 +0530

    Mileston-project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["project4/*.html"],
+  content: ["project3,project1,project-2,project4,projrct5/*.html"],
   theme: {
     extend: {},
 
